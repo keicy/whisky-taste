247e6db1b73a97d5d4349638808203398e2ceacc
keicy <kei.nakamura97@gmail.com>
2017-07-27 05:01:14 +0900

    (#8) Refactor evolution table order

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -1,17 +1,17 @@
-# Whiskies
+# Reviews
 
 # --- !Ups
-CREATE TABLE Whiskies (
-      whisky_id IDENTITY(1)
-    , whisky_name VARCHAR(100) NOT NULL
-    , distillery_name VARCHAR(100)
-    , country VARCHAR(50)
-    , region VARCHAR(50)
-    , type VARCHAR(50)
-    , strength DOUBLE
+CREATE TABLE Reviews (
+      review_id IDENTITY(1)
+    , user_id BIGINT NOT NULL
+    , whisky_id BIGINT NOT NULL
     , posted_date DATE DEFAULT CURRENT_DATE() NOT NULL
-    , PRIMARY KEY (whisky_id)
+    , title VARCHAR(50) NOT NULL
+    , score TINYINT UNSIGNED DEFAULT 10 NOT NULL
+            CHECK score BETWEEN 0 AND 20
+    , comment VARCHAR(1000)
+    , PRIMARY KEY (review_id)
 );
 
 # --- !Downs
-DROP TABLE Whiskies;
+DROP TABLE Reviews;
