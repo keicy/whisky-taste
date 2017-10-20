a416c883a38e490efa2401623b084b7099b7ceb9
keicy <kei.nakamura97@gmail.com>
2017-10-20 17:42:40 +0900

    (#11) Remove unneccesary codes

--- riot/store.js
+++ riot/store.js
@@ -4,7 +4,7 @@ import Action from './constants/action.js'
 import ac from './action-creator.js'
 import StoreMessage from './constants/store-message.js'
 
-/* TODO オブジェクトとして定義したほうが良さそう */
+/* TODO オブジェクトとして定義したほうが良いかも？ */
 
 const store = riot.observable()
 
@@ -22,9 +22,8 @@ function setActionHandler (
 setActionHandler(
   Action.INIT_STORE,
   StoreMessage.STORE_INITED,
-  initData => {
-    store.data = initData
-  })
+  initData => { store.data = initData }
+)
 
 /*
 setActionHandler(
@@ -38,46 +37,7 @@ setActionHandler(
 setActionHandler(
   Action.POST_NEW_REVIEW,
   StoreMessage.REVIEWS_UPDATED,
-  newReview => {
-    store.data.reviews.push(newReview)
-    console.log(store.data.reviews)
-  })
+  newReview => store.data.reviews.push(newReview)
+)
 
 export default store
-
-
-
-
-
-
-
-// var data = {
-//   reviews: [],
-// }
-// 
-// // TODO 可変長引数対応 `...data`
-// function setActionHandler (action, updateFn) {
-//   ac.on(action, data => {
-//     updateFn(data)
-//   })
-// }
-// 
-// setActionHandler(Action.GET_ALL_REVIEWS, reviews => {
-//   console.log(reviews)
-//   /*
-//    * 代入してもとObjとちがくなるとのと動かなくなるっぽい
-//    * やはり根本的な対応が必要そう
-//    */
-//   // data.reviews = reviews // => NG. 追加も動かない.
-//   // data = Object.assign(data, {reviews}) // NG. 追加も動かない.
-//   // data.reviews.concat(reviews) // => NG.
-//   reviews.forEach((review) => data.reviews.push(review)) // => おしい.
-//   console.log(data.reviews)
-// })
-// 
-// setActionHandler(Action.POST_NEW_REVIEW, newReview => {
-//   data.reviews.push(newReview) // => OK
-//   // data.reviews = [newReview] // => NG
-// })
-// 
-// export default data
