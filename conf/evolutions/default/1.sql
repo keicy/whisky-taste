a2dd144675c1c556ab8fd669c6bb7e8d3473f892
keicy <kei.nakamura97@gmail.com>
2017-10-30 23:39:01 +0900

    (#70) Update initial DB data

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -10,13 +10,14 @@ CREATE TABLE whiskies (
     , region VARCHAR(30)
 --    , type VARCHAR(50)
     , strength REAL
---    , posted_date DATE DEFAULT CURRENT_DATE() NOT NULL
 --    , created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 --    , updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
 );
 
 INSERT INTO whiskies (whisky_id, whisky_name, distillery_name, country, region, strength) VALUES (1, 'Knockando 21 yo 1990', 'Knockando', 'Scotland', 'Speyside', 43.0);
 INSERT INTO whiskies (whisky_id, whisky_name, distillery_name, country, region, strength) VALUES (2, 'GlenDronach 21 yo Parliament', 'The GlenDronach', 'Scotland', 'Speyside', 48.0);
+INSERT INTO whiskies (whisky_id, whisky_name, distillery_name, country, region, strength) VALUES (3, 'Auchentoshan 12 yo', 'Auchentoshan', 'Scotland', 'Lowland', 40.0);
+INSERT INTO whiskies (whisky_id, whisky_name, distillery_name, country, region, strength) VALUES (4, '山崎 12年', '山崎蒸溜所', '日本', '大阪', 43.0);
 SELECT setval('whiskies_whisky_id_seq', (SELECT MAX(whisky_id) FROM whiskies)); -- 上記でPKの値を指定して登録しているため,シーケンスオブジェクトの値を補正
 
 # --- !Downs
--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -16,8 +16,12 @@ CREATE TABLE reviews (
 --    , updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
 );
 
-INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (1, 1, 17, 'I love it! Aftertaste sustains long long long...');
-INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (2, 2, 17, 'Smooth and rich! Chocolate, wood, spicy.');
+INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (1, 1, 18, '麦の甘み、草の匂い、樹木の薫りが渾然と広がる。奥からはシェリーが漂う。そして訪れる素晴らしい余韻。長く長く、いつまでも続く。');
+INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (2, 2, 17, 'Smooth and rich! Chocolate, wood, spice.');
+INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (3, 2, 18, '甘美でほろ苦い、シェリーの魅力に溢れる恍惚の一本。');
+INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (4, 3, 14, 'My favorite daily bottle! Pretty good aroma. Smooth, woodiness, syrup, marmalade, vanilla.');
+INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (5, 3, 15, '森林に抱かれる心地の一本。舌に残る後味が白眉。長熟ボトルはさらに素晴らしい。');
+INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (6, 4, 12, 'シェリーの風味にオークのバニラ、樽香が溶け合う心地よい一本。少々値が張る。');
 SELECT setval('reviews_review_id_seq', (SELECT MAX(review_id) FROM reviews)); -- 上記でPKの値を指定して登録しているため,シーケンスオブジェクトの値を補正
 
 # --- !Downs
