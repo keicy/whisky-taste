05ee56a21d9dc78502f939f0f55008c25fd65493
keicy <kei.nakamura97@gmail.com>
2017-10-30 23:47:46 +0900

    (#70) Update initial DB data

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -21,7 +21,8 @@ INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (2, 2, 17, 'Sm
 INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (3, 2, 18, '甘美でほろ苦い、シェリーの魅力に溢れる恍惚の一本。');
 INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (4, 3, 14, 'My favorite daily bottle! Pretty good aroma. Smooth, woodiness, syrup, marmalade, vanilla.');
 INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (5, 3, 15, '森林に抱かれる心地の一本。舌に残る後味が白眉。長熟ボトルはさらに素晴らしい。');
-INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (6, 4, 12, 'シェリーの風味にオークのバニラ、樽香が溶け合う心地よい一本。少々値が張る。');
+INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (6, 4, 10, 'いつもの一本。スモーキーで甘い僕の親友。ロックが最高！');
+INSERT INTO reviews (review_id, whisky_id, score, comment) VALUES (7, 5, 12, 'シェリーの風味にオークのバニラ、樽香が溶け合う心地よい一本。少々値が張る。');
 SELECT setval('reviews_review_id_seq', (SELECT MAX(review_id) FROM reviews)); -- 上記でPKの値を指定して登録しているため,シーケンスオブジェクトの値を補正
 
 # --- !Downs
